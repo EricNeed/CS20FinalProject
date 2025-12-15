176ab6f5b8cf5779d03bdbc391b5187b0675622e
Eric Ni <6600271@spsd.sk.ca>
2025-12-15 15:21:17 -0600

    hi

--- src/client_scripts/tick_client.cpp
+++ src/client_scripts/tick_client.cpp
@@ -2,12 +2,19 @@
 #include<server/client_handler.h>
 #include<client/player_input.h>
 
-TickClient::TickClient() : client_handler(ClientHandler::getInstance()), spriteID(client_handler.newConnectedClient()), input_handler(spriteID){
+
+
+// TickClient::TickClient() : client_handler(ClientHandler::getInstance()), spriteID(client_handler.newConnectedClient()), input_handler(spriteID){
+// }
+
+TickClient::TickClient() : client_handler(ClientHandler::getInstance()), spriteID(client_handler.newConnectedClient()){
+    ClientInput skibidi_handler(spriteID);
+    input_handler = &skibidi_handler;
 }
 
 void TickClient::tick_client(){
     //handle user input
-    input_handler.processUserInput();
+    input_handler->processUserInput();
 
     //tick rendering
     client_rendering.tick_render();
